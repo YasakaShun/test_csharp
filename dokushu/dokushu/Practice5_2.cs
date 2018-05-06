9879a48a5af09eb550000608ccc1e3f120d16c08
YasakaShun <sdshvedujbfxye@gmail.com>
2018-05-06 20:11:52 +0900

    プロジェクト作成、練習問題5.2

---
+++ dokushu/dokushu/Practice5_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace dokushu
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Practice5_2
+    {
+        public Practice5_2()
+        {
+        }
+
+        public static void Exec1()
+        {
+            var str = "住所は〒184-0000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 嬬小石大野町0-9-9ですね。";
+            var matches = Regex.Matches(str, @"\d{3}-\d{4}");
+            foreach(var m in matches)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        public static void Exec2()
+        {
+            var str = "お問い合わせはhoge@example.comまで";
+            var rgx = new Regex(@"[a-z_0-9]+([-+.][a-z_0-9]+)*@[a-z_0-9]+([-.][a-z_0-9]+)*\.[a-z_0-9]+([-.][a-z_0-9]+)*");
+            Console.WriteLine(rgx.Replace(str, "<a href=\"mailto:$0\">$0</a>"));
+        }
+    }
+}
---
+++ dokushu/dokushu/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dokushu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kind kind = Kind.P5_2_2;
+
+            switch (kind)
+            {
+                case Kind.P5_2_1: Practice5_2.Exec1(); break;
+                case Kind.P5_2_2: Practice5_2.Exec2(); break;
+                default: break;
+            }
+
+            return;
+        }
+
+        private enum Kind
+        {
+            P5_2_1,
+            P5_2_2,
+        }
+    }
+}
